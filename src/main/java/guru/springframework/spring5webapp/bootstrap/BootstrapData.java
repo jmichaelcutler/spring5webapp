807ee0dfc43b5a1be82799f0f28668a2a98f7909
Jason Cutler <jmichaelcutler@gmail.com>
2022-04-15 15:27:50 -0500

    Up to assignment 2

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -0,0 +1,58 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootstrapData(
+            AuthorRepository authorRepository,
+            BookRepository bookRepository,
+            PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Publisher randomHouse = new Publisher();
+        publisherRepository.save(randomHouse);
+
+        Author timothy = new Author("Timothy", "Zahn");
+        Book lastCommand = new Book("The Last Command", "123123");
+        timothy.getBooks().add(lastCommand);
+        lastCommand.getAuthors().add(timothy);
+        lastCommand.setPublisher(randomHouse);
+
+        authorRepository.save(timothy);
+        bookRepository.save(lastCommand);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "456456");
+        noEJB.setPublisher(randomHouse);
+
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+        publisherRepository.save(randomHouse);
+
+
+
+
+    }
+}
